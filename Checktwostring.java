164214aab0db422a192d9708c37a7e056e3561c7
saravana <saravanasaru14313@gmail.com>
2019-06-07 16:27:19 +0530

    Update Checktwostring.java

--- Checktwostring.java
+++ Checktwostring.java
@@ -1,27 +1,27 @@
+import java.io.*;
 import java.util.*;
 public class Checktwostring
 {
 	public static void main(String[] args) 
 	{
-	    Scanner input=new Scanner(System.in);
-        String str1=input.nextLine();
-        String str2=input.nextLine();
-        int count=0;
-        for(int i=0;i<str1.length();i++)
-        {
-            if(str1.charAt(i)!=str2.charAt(i))
-            {
-                count++;    
-            }
-        }
-        if(count==1)
-        {
-            System.out.println("yes");
-        }
-        else
-        {
-            System.out.println("no");
-        }
+	    	Scanner input=new Scanner(System.in);
+        	String str1=input.nextLine();
+        	String str2=input.nextLine();
+        	int count=0;
+        	for(int i=0;i<str1.length();i++)
+        	{
+            		if(str1.charAt(i)!=str2.charAt(i))
+            		{
+                		count++;    
+            		}
+        	}
+        	if(count==1)
+        	{
+            		System.out.println("yes");
+        	}
+        	else
+        	{
+            		System.out.println("no");
+        	}
 	}
 }
-        
